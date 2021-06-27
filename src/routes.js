f8705bba650585f5236a7779463c0949579075d9
Xiaomei Wu <xiaomei.wu@nexenio.com>
2021-06-27 11:49:36 +0200

    Add signin using formik and yup

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Home } from './components/Home';
+import { SignIn } from './components/SignIn';
 
 export const Routes = () => {
   return (
     <BrowserRouter>
       <Header />
       <Switch>
+        <Route path="/signin" exact component={SignIn} />
         <Route path="/" exact component={Home} />
       </Switch>
       <Footer />
