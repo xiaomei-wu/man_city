ca0ef8170cb8fe2cfb2e1b9d73d620bd027b4b3e
Xiaomei Wu <xiaomei.wu@nexenio.com>
2021-06-30 21:58:22 +0200

    Add data to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,8 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
+
+// import { cityDb } from './temp/m-city-export';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -15,4 +18,38 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-export { firebase };
+// const DB = firebase.firestore();
+// const matchesCollection = DB.collection('matches');
+// const playersCollection = DB.collection('players');
+// const positionsCollection = DB.collection('positions');
+// const promotionsCollection = DB.collection('promotions');
+// const teamsCollection = DB.collection('teams');
+
+// cityDb.matches.forEach(item => {
+//   matchesCollection.add(item);
+// });
+//
+// cityDb.players.forEach(item => {
+//   playersCollection.add(item);
+// });
+//
+// cityDb.positions.forEach(item => {
+//   positionsCollection.add(item);
+// });
+//
+// cityDb.promotions.forEach(item => {
+//   promotionsCollection.add(item);
+// });
+//
+// cityDb.teams.forEach(item => {
+//   teamsCollection.add(item);
+// });
+
+export {
+  firebase,
+  // matchesCollection,
+  // playersCollection,
+  // positionsCollection,
+  // promotionsCollection,
+  // teamsCollection,
+};
